fb485884b7c70fda3b46c1f887459049e32f819a
Andreas Delabie <andreas.delabie@gmail.com>
2025-03-06 19:30:08 +0100

    Add files via upload

---
+++ clear.bat
@@ -0,0 +1,9 @@
+del /s /f /q %userprofile%\Recent\*.*
+
+del /s /f /q C:\Windows\Prefetch\*.*
+
+del /s /f /q C:\Windows\Temp\*.*
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+
+del /s /f /q %USERPROFILE%\AppData\Roaming\stremio\stremio-server\stremio-cache
